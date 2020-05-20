b7aa9bd24116dee81082baeb205f65bca76a07ea
vncscampos <viniolic2018@gmail.com>
2020-05-20 11:26:27 -0300

    frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes')
 
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true, 
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
